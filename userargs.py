153f08881126fca5106e8fdeb0c3e0a4fee59714
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-29 02:45:12 +0000

    Adding tarfile member sanitization to extractall()

--- userargs.py
+++ userargs.py
@@ -979,7 +979,26 @@ def set_up_postnovo(
 
             print('Unzipping ' + denovogui_dl_filename)
             with tarfile.open(denovogui_dl_filename, 'r:gz') as f:
-                f.extractall(config.postnovo_dir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(f, config.postnovo_dir)
             os.remove(denovogui_dl_filename)
 
             #Update the user download record.
